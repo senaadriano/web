afcf644b97b9bdf3b7313e98a10c5281a9fa7f6e
Adriano Sena <adrianosena013@gmail.com>
2023-09-06 23:18:59 -0300

    final commit

---
+++ script.js
@@ -0,0 +1,29 @@
+//variáveis
+var a = 10;
+var b = 5;
+var c = 1;
+var d;
+
+// Operações
+c = b--;
+b = a++;
+a = 5 + "1";
+d = "1" + "1";
+
+// Imprimindo os valores
+console.log("Valor de a: " + a);
+console.log("Valor de b: " + b);
+console.log("Valor de c: " + c);
+console.log("Valor de d: " + d);
+
+// informações
+var autor = "Adriano de Sena Mendonça";
+var palavraChave = "JavaScript";
+var descricao = "Atividades sobre JavaScript";
+var tituloPagina = "JavaScript";
+
+// Imprimindo as informações
+console.log("Autor: " + autor);
+console.log("Palavra-chave: " + palavraChave);
+console.log("Descrição: " + descricao);
+console.log("Título da Página: " + tituloPagina);
